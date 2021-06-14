87b892eb8510fe605a844163fae218c7948df6eb
RajeshKumarC6 <rajeshkumaretldev@gmail.com>
2021-06-14 15:15:06 +0000

    woodle

---
+++ src/main/java/woodleapp/App.java
@@ -0,0 +1,13 @@
+package woodleapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
